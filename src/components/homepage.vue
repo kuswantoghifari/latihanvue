861c4eb2a652a7d5f6b0a4b0e5ec96f082495e7d
kuswantoghifari <blazehawk64@gmail.com>
2018-03-14 16:21:36 +0700

    commit awal

---
+++ src/components/homepage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="test">
+      <button v-on:click= "aw">pencet saya</button>
+  </div>
+</template>
+<script>
+export default {
+    
+        methods: {
+            aw : function(){
+                console.log('aw bang jangan tekan saya')
+                alert('malah di pencet')
+
+        }
+
+    }
+
+    };
+    
+</script>
+
